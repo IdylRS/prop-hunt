5db9f8f7ef448cc31c00f7ffff08905c4d9d50fe
IdylRS <will@idyl.live>
2022-09-11 18:40:20 -0600

    Add new models

--- src/main/java/com/idyl/prophunt/PropHuntModelId.java
+++ src/main/java/com/idyl/prophunt/PropHuntModelId.java
@@ -6,7 +6,17 @@ public enum PropHuntModelId {
     TREE(1637),
     CRATE(15402),
     ROCK_PILE(1391),
-    HAT_STAND(1185);
+    HAT_STAND(1185),
+    CHEST(11204),
+    BED(1147),
+    STOOL(10089),
+    UNDOR(10321),
+    BANK(10647),
+    POTTED_PLANT(1632),
+    LIGHT(10941),
+    ROPE(11442),
+    SPADE(9556),
+    SIGN(9555);
 
     private final int value;
 
