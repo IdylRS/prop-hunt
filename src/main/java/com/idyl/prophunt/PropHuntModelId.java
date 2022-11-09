9480b50f2dd07f794feba39da2a81fb99128eff8
IdylRS <will@idyl.live>
2022-11-09 10:26:31 -0700

    Add new model ids

--- src/main/java/com/idyl/prophunt/PropHuntModelId.java
+++ src/main/java/com/idyl/prophunt/PropHuntModelId.java
@@ -8,13 +8,18 @@ public enum PropHuntModelId {
     CHEST(1249),
     STOOL(10089),
     POTTED_PLANT(1620),
-    SPADE(9556),
     MUSHROOM(11985),
     SKELETON(1078),
     SKELETON2(1080),
     PIPE(12472),
     BOULDER(15624),
-    CANNONBALLS(1550);
+    BOULDER2(20031),
+    CANNONBALLS(1550),
+    CACTUS(1580),
+    FERN(1618),
+    DEAD_TREE(1719),
+    DAGGER(2672),
+    BONES(2674);
 
 
     private final int value;
